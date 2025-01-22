93a9ea4533f71d0ce70a2702aa77bf7b185aebc2
Gavin Szczesniak <gavinszczesniak@gmail.com>
2025-01-22 15:50:56 -0500

    Pushes starting progress of the course. Followed along for more than half of the course

---
+++ src/app/user/user.component.ts
@@ -0,0 +1,30 @@
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { User } from './user.model';
+
+// type User = {
+//   id: string;
+//   name: string;
+//   avatar: string;
+// };
+
+@Component({
+  selector: 'app-user',
+  standalone: true,
+  imports: [],
+  templateUrl: './user.component.html',
+  styleUrl: './user.component.css'
+})
+
+export class UserComponent {
+  @Input({ required: true }) user!: User;
+  @Input({ required: true }) isSelected!: boolean;
+  @Output() select = new EventEmitter<string>();
+
+  get imagePath() {
+    return `assets/users/${this.user.avatar}`;
+  }
+
+  onSelectUser() {
+    this.select.emit(this.user.id);
+  }
+}
